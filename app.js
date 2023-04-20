0de3d939a7dce77d1ae9388f8c22dd05562bb630
Karl <toom.karl42@gmail.com>
2023-04-20 16:46:08 +0300

    created a promise to show video depending on subscription status

--- app.js
+++ app.js
@@ -1,16 +1,36 @@
-const statusRef = document.querySelector(".status")
+const statusRef = document.querySelector(".status");
 
 function getSubscriptionStatus() {
   return new Promise((resolve, reject) => {
-    resolve("VIP");
+    resolve(undefined);
   });
 }
-// 1.Then
+/*// 1.Then
 //getSubscriptionStatus().then((response) => console.log(response));
 
 // 2. Async/Await
 async function main() {
   const status = await getSubscriptionStatus()
   statusRef.innerHTML = status
+}
+main();
+*/
+
+function getVideo(subscriptionStatus) {
+  return new Promise((resolve, reject) => {
+    if (subscriptionStatus === "VIP") {
+      resolve("show video");
+    } else if (subscriptionStatus === "FREE") {
+      resolve("show trailer");
+    } else {
+      reject("no video");
+    }
+  });
+}
+
+async function main() {
+  const status = await getSubscriptionStatus();
+  statusRef.innerHTML = status;
+  console.log(await getVideo(status));
 }
 main();
