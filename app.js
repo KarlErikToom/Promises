51398be5712f25a06bb3e6f909d71df920d3edc8
Karl <toom.karl42@gmail.com>
2023-04-20 16:20:16 +0300

    creating a promise

--- app.js
+++ app.js
@@ -1,27 +1,16 @@
-//console.log(fetch("https://jsonplaceholder.typicode.com/users/1"))
-const emailRef = document.querySelector(".email");
+const statusRef = document.querySelector(".status")
 
-// 1. Then
-
-/*fetch("https://jsonplaceholder.typicode.com/users/1")
-  .then((respone) => {
-    return respone.json();
-  })
-  .then((data) => {
-    console.log(data);
-    emailRef.innerHTML = data.email
+function getSubscriptionStatus() {
+  return new Promise((resolve, reject) => {
+    resolve("VIP");
   });
-  
-*/
-
-
+}
+// 1.Then
+//getSubscriptionStatus().then((response) => console.log(response));
 
 // 2. Async/Await
-
-async function main(){
-    const response = await fetch("https://jsonplaceholder.typicode.com/users/1")
-    const data = await response.json()
-    console.log(data)
-    emailRef.innerHTML = data.email
+async function main() {
+  const status = await getSubscriptionStatus()
+  statusRef.innerHTML = status
 }
-main()
+main();
